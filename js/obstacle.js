096a9687a2d7585d0fb0489ba2b461bdff0fe533
Jonathan Hingorani <jonathanhingorani@gmail.com>
2024-05-26 18:49:00 +0200

    done

---
+++ js/obstacle.js
@@ -0,0 +1,27 @@
+class Obstacle {
+    constructor(gameScreen, gameSpeed) {
+      this.gameScreen = gameScreen;
+      this.positionsArr = [85, 300];
+      this.randomIndex = Math.floor(Math.random() * this.positionsArr.length);
+      this.left = this.positionsArr[this.randomIndex];
+      this.top = -250;
+      this.width = 100;
+      this.height = 150;
+      this.element = document.createElement("img");
+      this.element.src = "/images/redCar.png";
+      this.element.style.position = "absolute";
+      this.element.style.top = `${this.top}px`;
+      this.element.style.left = `${this.left}px`;
+      this.element.style.height = `${this.height}px`;
+      this.element.style.width = `${this.width}px`;
+      this.gameScreen.appendChild(this.element);
+      this.gameSpeed = gameSpeed;
+    }
+    move() {
+      this.top += Number(this.gameSpeed);
+      this.updatePosition();
+    }
+    updatePosition() {
+      this.element.style.top = `${this.top}px`;
+    }
+  }
